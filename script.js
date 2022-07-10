b7190e6362de29b6fe03c019fb5a673b27ac0fa9
Gustavoddr <alexandregustavo217@gmail.com>
2022-07-10 02:19:09 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const init = () => {
+    const inputEmail = document.querySelector("#email");
+    const inputpassword = document.querySelector("#password");
+    const inputsubmit = document.querySelector("#button");
+
+    if(inputsubmit) {
+        inputsubmit addEventListener('click', (event) => {
+            event.preventDefault();
+
+            fetch( {
+
+            })
+        })
+    }
+}
+window.onload = init;
